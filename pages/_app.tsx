1ee431f544963e93049b28c4d13f6b0c6251cae7
bandyg <bandyg@gmail.com>
2023-07-23 18:57:47 +0800

    1 file modified, 1 added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+const databaseUrl = process.env.DATABASE_URL;
+const apiKey = process.env.API_KEY;
+const secretToken = process.env.SECRET_TOKEN;
+console.log(`db: ${databaseUrl}, apikey: ${apiKey}, st: ${secretToken}`);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
