9638d839b8cba01ff60d57e4553a37e3e059e6a5
bandyg <bandyg@gmail.com>
2023-07-26 07:51:33 +0800

    add Nav 1

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -6,9 +6,12 @@ const Nav = (props) => {
     props.setShowElement(!props.ShowElement);
   };
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div className="flex flex-row bg-black text-white">
+      <div>home</div>
+      <div>projects</div>
+      <div>about</div>
+      <div>contact</div>
+    </div>
   );
 };
 export default Nav;
