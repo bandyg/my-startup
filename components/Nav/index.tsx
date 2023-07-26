eb9e1deb5fc914f84809f2714d574c8366fe79b1
bandyg <bandyg@gmail.com>
2023-07-26 21:46:26 +0800

    1 file modified

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -6,11 +6,19 @@ const Nav = (props) => {
     props.setShowElement(!props.ShowElement);
   };
   return (
-    <div className="flex flex-row justify-evenly bg-blue-500 text-black">
-      <div>home</div>
-      <div>projects</div>
-      <div>about</div>
-      <div>contact</div>
+    <div className="flex flex-row bg-blue-500 text-black justify-around">
+      <div className="flex basis-1/4 justify-evenly ">
+        <div>home</div>
+        <div>projects</div>
+        <div>about</div>
+        <div>contact</div>
+      </div>
+      <div className="flex grow justify-evenly">
+        <div>icon</div>
+      </div>
+      <div className="flex basis-1/4 justify-evenly">
+        <div>links</div>
+      </div>
     </div>
   );
 };
